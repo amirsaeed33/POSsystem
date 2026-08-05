dde753f4c4353d2f709da1f8ecfc048089f1fd4f
Muhammad Amir Saeed <amirdev639@gmail.com>
2026-08-05 07:23:22 +0500

    add migration for removing the company profile

---
+++ aspnet-core/src/SmartPos.EntityFrameworkCore/Migrations/20260805022305_Removed_CompanyProfiles.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartPos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removed_CompanyProfiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppCompanyProfiles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppCompanyProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ImagePath = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true),
+                    InvoiceAddress = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true),
+                    InvoiceContactEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    InvoiceContactPhone = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: true),
+                    InvoiceFooter = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    TaxNumber = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    TenantId = table.Column<int>(type: "int", nullable: true),
+                    Website = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppCompanyProfiles", x => x.Id);
+                });
+        }
+    }
+}
