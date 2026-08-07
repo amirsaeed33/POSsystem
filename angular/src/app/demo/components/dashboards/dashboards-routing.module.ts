ddec5d59f6908322bffadb40072604e7e115e292
Muhammad Amir Saeed <amirdev639@gmail.com>
2026-08-08 02:44:46 +0500

    update routing

--- angular/src/app/demo/components/dashboards/dashboards-routing.module.ts
+++ angular/src/app/demo/components/dashboards/dashboards-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [RouterModule.forChild([
-        { path: '', data: {breadcrumb: 'SaaS Dashboard'}, loadChildren: () => import('./saas/saas.dashboard.module').then(m => m.SaaSDashboardModule) },
+        { path: '', data: {breadcrumb: 'Dashboard'}, loadChildren: () => import('./saas/saas.dashboard.module').then(m => m.SaaSDashboardModule) },
         { path: 'dashboard-sales', data: {breadcrumb: 'Sales Dashboard'}, loadChildren: () => import('./sales/sales.dashboard.module').then(m => m.SalesDashboardModule) }
     ])],
     exports: [RouterModule]
